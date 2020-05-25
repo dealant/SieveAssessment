50a7c295689a99e774fd44c6b6cc544fa82bce0f
alansuface <alansuface@192.168.86.33>
2020-05-24 19:45:53 -0700

    almost done with sieve

--- SieveOfEratosthenes/src/Sieve.java
+++ SieveOfEratosthenes/src/Sieve.java
@@ -8,20 +8,43 @@ import java.util.*;
  */
 public class Sieve {
 	private Queue<Integer> candidates = new LinkedList<Integer>();
-	//TODO Create an empty queue to store primes.
 	private Queue<Integer> prime = new LinkedList<Integer>();
+	private int max = 0;
 	
-	public Sieve(int n) {
-		candidates.clear();
-		prime.clear();
+	public Sieve() {
+		clearQueue();
 	}
 	
 	public void computeTo(int n) {
-		//TODO clear both queues
+		clearQueue();
+		max = n;
 		//Create a queue and fill it with the consecutive integers 2 through n inclusive. 
 		for(int i = 2; i<=n; i++) {
 			candidates.add(i);
 		}
+		prime.add(2);
+		int current = 2;
+		do {
+			int size = candidates.size();
+			for(int i = 0; i<size; i++){
+				if(candidates.peek()%current==0) {
+					System.out.print(" c:" + candidates.peek());
+					System.out.print(" current: "+current);
+					candidates.remove();
+				}else {
+					candidates.add(candidates.poll());
+				}
+			}
+			System.out.println();
+			if(!prime.contains(current)) {
+				prime.add(current);
+			}
+			current = candidates.poll();
+			System.out.println(prime.toString());
+			System.out.println(candidates.toString());
+//		} while(candidates.peek()<7);
+		} while(candidates.peek()<= Math.sqrt(n)+3);
+		prime.addAll(candidates);
 	/*
 	Do
 		Obtain the next prime p by removing the first value in the queue of numbers.
@@ -33,15 +56,21 @@ public class Sieve {
 	}
 	
 	public void reportResults() {
-		
+		System.out.println(prime.toString());
 	}
 	
-	public void getMax() {
-		
+	public int getMax() {
+		//TODO set if max is 0 throw exception
+		return max;
 	}
 	
-	public void getCount() {
-		
+	public int getCount() {
+		return prime.size();
+	}
+	
+	private void clearQueue() {
+		candidates.clear();
+		prime.clear();
 	}
 	
 }
